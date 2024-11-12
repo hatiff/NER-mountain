bfa691a3d821424bb54bbd1ef0ddf96f16e44658
Olexii <stexmachina@gmail.com>
2024-11-12 13:17:56 +0200

    fix

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def train_model(path):
         learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        num_train_epochs=5,
+        num_train_epochs=3,
         weight_decay=0.01,
 
     )
